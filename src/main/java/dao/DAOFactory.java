19be211d02088702d340a8494594228e87284920
Perabout <florine.perabout@gmail.com>
2024-10-08 10:44:28 +0200

    DAO et DAOFactory OK

--- src/main/java/dao/DAOFactory.java
+++ src/main/java/dao/DAOFactory.java
@@ -4,10 +4,29 @@
  */
 package dao;
 
-/**
- *
- * @author stag
- */
 public class DAOFactory {
-    
+    private static final DAOPerson daopers = new DAOPerson();
+    private static final DAOStory daostory = new DAOStory();
+    private static final DAOComment daocomment = new DAOComment();
+    private static final DAOVote daovote = new DAOVote();
+
+    private DAOFactory() {
+    }
+
+    public static DAOPerson getDAOPerson() {
+        return daopers;
+    }
+
+    public static DAOStory getDAOStory() {
+        return daostory;
+    }
+
+    public static DAOComment getDAOComment() {
+        return daocomment;
+    }
+
+    public static DAOVote getDAOVote() {
+        return daovote;
+    }
+
 }
