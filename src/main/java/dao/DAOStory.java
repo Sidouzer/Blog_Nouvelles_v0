6dfb2e2d8efdd7cd40c8394bc0bf818b402058b6
sid <sid.ksouri@gmail.com>
2024-10-08 14:00:11 +0200

    modif Story, DAO, DAOStory, Crudable

--- src/main/java/dao/DAOStory.java
+++ src/main/java/dao/DAOStory.java
@@ -9,7 +9,7 @@ import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class DAOStory extends DAO<Story> {
+public class DAOStory extends DAO<Story>  {
 
     // Constructeur, on passe le nom de la table en paramètre
     public DAOStory() {
