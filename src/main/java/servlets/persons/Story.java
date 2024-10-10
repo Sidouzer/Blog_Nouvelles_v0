a6cd008807cfcadd290c5b3f60c608639324b7d8
Perabout <florine.perabout@gmail.com>
2024-10-10 15:43:43 +0200

    story.jsp amélioré icons like et dislike DAOVote amélioré BackVote ok Servlet story début

--- src/main/java/servlets/persons/Story.java
+++ src/main/java/servlets/persons/Story.java
@@ -1,6 +1,8 @@
-package servlets.persons;   //SID
+package servlets.persons;
 
 import dao.DAOFactory;
+import dao.DAOVote;
+import jakarta.faces.application.ViewVisitOption;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,12 +12,16 @@ import java.io.IOException;
 import java.util.Collection;
 
 /**
- * Servlet pour afficher la liste des histoires.
+ * authors: Sid et Florine
+ *
  */
 @WebServlet(urlPatterns = "/stories")
 public class Story extends HttpServlet {
+    Long id;
+    int quality;
     /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -23,14 +29,35 @@ public class Story extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+        throws ServletException, IOException {
         // Récupération de la liste des histoires depuis la base de données
         Collection<beans.Story> stories = DAOFactory.getDAOStory().all();
-
-        // Ajout de la liste des histoires à l'objet request
-        request.setAttribute("stories", stories);
-
-        // Redirection vers la page JSP pour afficher les histoires
+        String idParam = request.getParameter("id");
+        try {
+            id = Long.valueOf(idParam);
+        } catch (NumberFormatException ex) {
+            id = null;
+        }
+        if (id != null) {
+            request.setAttribute(
+                "quality",
+                DAOFactory.getDAOVote().findByQuality(0));
+            // Ajout de la liste des histoires à l'objet request
+            request.setAttribute("stories", stories);
+        }
+        getServletContext().getRequestDispatcher(VIEW)
+            .forward(request, response);
         request.getRequestDispatcher("/WEB-INF/jsp/story.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        throws ServletException, IOException {
+        //je veux que mon dopost renvoi mon vote.quality dans countLikeByIdStory
+        // mon countLikeByIdStory enregiste le nombre de votes
+        //il prend le nombre de Like(quality 1) et divise le nb de Like par le nb de votes
+        int totalLikes = DAOVote.countLikeByIdStory(id, quality);
+
+        }
     }
 }
