33c6d2925a184911b273bf3a52d691880f98597f
Perabout <florine.perabout@gmail.com>
2024-10-10 10:57:31 +0200

    CreateCommentFormChecker ok

--- src/main/java/work/CreateCommentFormChecker.java
+++ src/main/java/work/CreateCommentFormChecker.java
@@ -5,7 +5,9 @@
 package work;
 
 import beans.Comment;
+import beans.Person;
 import jakarta.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
 
 /**
  *
@@ -13,13 +15,30 @@ import jakarta.servlet.http.HttpServletRequest;
  */
 public class CreateCommentFormChecker extends FormChecker<Comment> {
 
+    Person person = new Person();
+
     public CreateCommentFormChecker(HttpServletRequest request) {
         super(request, new Comment());
+        person = (Person) request.getSession().getAttribute("person");
     }
 
     @Override
     public Comment checkForm() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String title = request.getParameter("title").trim();
+        String content = request.getParameter("content").trim();
+        bean.setTitle(title);
+        bean.setContent(content);
+        bean.setId_person(person.getId());
+        bean.setCreated(LocalDate.now());
+        if (title.length() > 20) {
+            errors.put("title",
+                new RuntimeException("Titre trop long (20 caractères maximum"));
+        }
+        if (content.length() > 150) {
+            errors.put("content",
+                new RuntimeException("Contenu trop long (150 caractères maximum"));
+        }
+        return bean;
     }
 
 }
