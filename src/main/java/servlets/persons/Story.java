b238e0cd7359e41ee2be94647b05f6e51f6e3fc0
Caroline Casteras <casterascaroline@gmail.com>
2024-10-10 18:57:16 +0200

    Modification des chemins pour que l'administrateur ait accès au back Modification des chemins d'accès pour qu'a l'inscription on se retrouve dans le profil

--- src/main/java/servlets/persons/Story.java
+++ src/main/java/servlets/persons/Story.java
@@ -17,47 +17,47 @@ import java.util.Collection;
  */
 @WebServlet(urlPatterns = "/stories")
 public class Story extends HttpServlet {
-    Long id;
-    int quality;
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        // Récupération de la liste des histoires depuis la base de données
-        Collection<beans.Story> stories = DAOFactory.getDAOStory().all();
-        String idParam = request.getParameter("id");
-        try {
-            id = Long.valueOf(idParam);
-        } catch (NumberFormatException ex) {
-            id = null;
-        }
-        if (id != null) {
-            request.setAttribute(
-                "quality",
-                DAOFactory.getDAOVote().findByQuality(0));
-            // Ajout de la liste des histoires à l'objet request
-            request.setAttribute("stories", stories);
-        }
-        getServletContext().getRequestDispatcher(VIEW)
-            .forward(request, response);
-        request.getRequestDispatcher("/WEB-INF/jsp/story.jsp").forward(request, response);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
-        throws ServletException, IOException {
-        //je veux que mon dopost renvoi mon vote.quality dans countLikeByIdStory
-        // mon countLikeByIdStory enregiste le nombre de votes
-        //il prend le nombre de Like(quality 1) et divise le nb de Like par le nb de votes
-        int totalLikes = DAOVote.countLikeByIdStory(id, quality);
-
-        }
-    }
+//    Long id;
+//    int quality;
+//    /**
+//     * Handles the HTTP <code>GET</code> method.
+//     *
+//     * @param request servlet request
+//     * @param response servlet response
+//     * @throws ServletException if a servlet-specific error occurs
+//     * @throws IOException if an I/O error occurs
+//     */
+//    @Override
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        // Récupération de la liste des histoires depuis la base de données
+//        Collection<beans.Story> stories = DAOFactory.getDAOStory().all();
+//        String idParam = request.getParameter("id");
+//        try {
+//            id = Long.valueOf(idParam);
+//        } catch (NumberFormatException ex) {
+//            id = null;
+//        }
+//        if (id != null) {
+//            request.setAttribute(
+//                "quality",
+//                DAOFactory.getDAOVote().findByQuality(0));
+//            // Ajout de la liste des histoires à l'objet request
+//            request.setAttribute("stories", stories);
+//        }
+//        getServletContext().getRequestDispatcher(VIEW)
+//            .forward(request, response);
+//        request.getRequestDispatcher("/WEB-INF/jsp/story.jsp").forward(request, response);
+//    }
+//
+//    @Override
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+//        throws ServletException, IOException {
+//        //je veux que mon dopost renvoi mon vote.quality dans countLikeByIdStory
+//        // mon countLikeByIdStory enregiste le nombre de votes
+//        //il prend le nombre de Like(quality 1) et divise le nb de Like par le nb de votes
+//        int totalLikes = DAOVote.countLikeByIdStory(id, quality);
+//
+//        }
+//    }
 }
