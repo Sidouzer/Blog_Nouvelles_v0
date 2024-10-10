0fd6ee0714af580ac5c62fc9b5e24ae77cc58091
Perabout <florine.perabout@gmail.com>
2024-10-10 10:30:39 +0200

    DAOComment refait CommentReport ok

--- src/main/java/work/CreateCommentFormChecker.java
+++ src/main/java/work/CreateCommentFormChecker.java
@@ -4,10 +4,22 @@
  */
 package work;
 
+import beans.Comment;
+import jakarta.servlet.http.HttpServletRequest;
+
 /**
  *
  * @author stag
  */
-public class CreateCommentFormChecker {
-    
+public class CreateCommentFormChecker extends FormChecker<Comment> {
+
+    public CreateCommentFormChecker(HttpServletRequest request) {
+        super(request, new Comment());
+    }
+
+    @Override
+    public Comment checkForm() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
 }
